8ad589e1d9041416d93fc532fd97e410b6cb64e1
Victor <victordiogo2801@gmail.com>
2025-12-30 12:14:02 -0300

    Mudança na criação do id

--- atividade2/funcionalidades/crateId.js
+++ atividade2/funcionalidades/crateId.js
@@ -1,6 +1,7 @@
-let id = 5
-function createId() {
-    id++
+
+function createId(estudantes) {
+    const posicaoMaxima = estudantes.findIndex(item => item.quantity === Math.max(...estudantes.map(i => i.quantity)));
+    let id = estudantes[posicaoMaxima].id + 1
     return id
 }
 module.exports = createId
